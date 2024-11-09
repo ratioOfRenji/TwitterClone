6d4b9cb216018b24de08fce33ca394a0640a6520
Taras Drabyazg <seedleep@gmail.com>
2024-11-09 10:19:55 +0300

    доработал переключение скринов

--- Assets/Scripts/UI/Blogs/BlogsScreensSwicher.cs
+++ Assets/Scripts/UI/Blogs/BlogsScreensSwicher.cs
@@ -8,6 +8,9 @@ public class BlogsScreensSwicher : MonoBehaviour
 	[SerializeField] private GameObject _blogsScreen;
 	[SerializeField] private GameObject _createBlogScreen;
 	[SerializeField] private GoogleAuthentication _googleAuthentication;
+	[SerializeField] private GameObject _headerFooterScreen;
+	[SerializeField] private GameObject _editProfileScreen;
+	[SerializeField] private GameObject _profileScreen;
 	private CompositeDisposable _disposables = new CompositeDisposable();
 	private void Awake()
 	{
@@ -16,9 +19,22 @@ public class BlogsScreensSwicher : MonoBehaviour
 	public void ShowBlogsScreen()
 	{
 		_createBlogScreen.SetActive(false);
+		_editProfileScreen.SetActive(false);
+		_profileScreen.SetActive(false);
 		_blogsScreen.SetActive(true);
+		_headerFooterScreen.SetActive(true);
 	}
 
+	public void ShowEditProfileScreen()
+	{
+		_editProfileScreen.SetActive(true);
+	}
+	public void ShowProfileScreen()
+	{
+		_editProfileScreen.SetActive(false);
+		_headerFooterScreen.SetActive(true);
+		_profileScreen.SetActive(true);
+	}
 	public void ShowCreateBlogScreen()
 	{
 		_createBlogScreen.SetActive(true);
